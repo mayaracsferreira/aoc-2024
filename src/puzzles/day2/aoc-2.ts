0705a6f1ed6181b2fb5857e267710b0f2ce30d26
Mayara Ferreira <mayara_csf@hotmail.com>
2024-12-04 22:06:54 -0300

    1# puzzle part 1 solution

---
+++ src/puzzles/day2/aoc-2.ts
@@ -0,0 +1,58 @@
+import { readFile } from "../../helpers/fileHelper";
+import * as path from "path";
+
+const SAMPLE_INPUT = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
+
+const FILENAME = path.join(__dirname, "./input2.txt");
+const INPUT = readFile(FILENAME);
+
+const reports = INPUT.split("\n").map((levels) =>
+  levels.split(" ").map(Number)
+);
+
+function isAllIncreasing(inputArray: number[]): boolean {
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] < inputArray[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isAllDecreasing(inputArray: number[]): boolean {
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] > inputArray[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isAdjascentDistanceValid(inputArray: number[]) {
+  const consecutiveLevesDistances = inputArray.slice(1).map((item, index) => {
+    return Math.abs(item - inputArray[index]);
+  });
+
+  const isDistanceValid = consecutiveLevesDistances.every(
+    (distance) => distance >= 1 && distance <= 3
+  );
+  return isDistanceValid;
+}
+
+function isSafe(inputArray: number[]): boolean {
+  const isSafe =
+    isAdjascentDistanceValid(inputArray) &&
+    (isAllIncreasing(inputArray) || isAllDecreasing(inputArray));
+  return isSafe;
+}
+
+let totalSafeReports: number = reports.reduce((total, level) => {
+  return total + (isSafe(level) ? 1 : 0);
+}, 0);
+
+console.log(`Total of safe levels ${totalSafeReports}`);
